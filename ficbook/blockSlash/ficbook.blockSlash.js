3c6f97db6a223f788ad1a054780916c7608a5b12
ShadowOfKing <ShadowKing65@mail.ru>
2018-02-06 20:40:27 +0500

    Модифицирован список возможных параметров
    Добавлено:
      delay - время до блокировки элемента. Полезно при не сразу появляющейся на странице рекламе.
      repeats - повторные блокировки элемента. Полезно если элемент появляется несколько раз, либо на разных страницах в разное время (например на одной - через 500мс, на другой - через 2с)
      step - шаг зажержки перед очередным повтором блокировки
      css - css правила для элемента. При желании можно добавить "display": "none" для убирания подсказки о скрытии элемента.
      
    
    Также добавлены блокировки рекламы и фиков с нелюбимыми жанрами.

--- ficbook/blockSlash/ficbook.blockSlash.js
+++ ficbook/blockSlash/ficbook.blockSlash.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BlockFicBookSlash
 // @namespace    https://github.com/ShadowOfKing/JSScripts/
-// @version      1.0
+// @version      1.1
 // @description  Скрывает на сайте фанфики, у которых в жанре стоит слэш.
 // @author       Wilat Collany
 // @match        https://ficbook.net/*
@@ -41,24 +41,73 @@ var blocks = {
         "message": "Тут был фемслэш",
         "block": true,
     },
+    "ads": {
+        "constraints":
+        [
+            {
+                "selector": "yatag",
+                "parent": "div"
+            },
+        ],
+        "message": "Тут была реклама",
+        "block": true,
+        "delay": 500,
+        "repeats": 3,
+        "step": 1000,
+        "css": {
+            "background-color": "#4d2917",
+            "color": "#f6ecda",
+        }
+    },
+    "genres": {
+        "constraints":
+        [
+            {
+                "selector": ".disliked-parameter-link",
+                "parent": "#main, article"
+            },
+        ],
+        "message": "Тут был нелюбимый жанр",
+        "block": true,
+        "css": {
+            "background-color": "#2c1a14",
+            "color": "#dd3131",
+        }
+    },
 };
 
 
 function blockElement(el) {
     var message = el.message;
-    for (var i = 0; i < el.constraints.length; i++) {
-        var con = el.constraints[i];
-        var $selector = $(con.selector);
-        $selector.each(function() {
-            var $parent = $(this).parents(con.parent).first();
-            $parent.wrap("<div></div>");
-            $parent = $parent.parent();
-            $parent.html("<div class='BlockContent'>" + message + "</div>");
-            $parent.children().click(function(e) {
-                $(this).addClass('closed');
-                $(this).fadeOut('medium');
-            });
-        });
+    var delay = el.delay == null ? 1 : el.delay;
+    var count = el.repeats == null ? 1 : el.repeats;
+    var step = el.step == null ? delay : el.step;
+    for (var iter = 0; iter < count; iter++) {
+        setTimeout(function() {
+            for (var i = 0; i < el.constraints.length; i++) {
+                var con = el.constraints[i];
+                var $selector = $(con.selector);
+                $selector.each(function() {
+                    var $parent = $(this).parents(con.parent).first();
+                    $parent.wrap("<div></div>");
+                    $parent = $parent.parent();
+                    var css = "";
+                    if (el.css != null) {
+                        css = 'style="';
+                        for (var j in el.css) {
+                            css += j + ': ' + el.css[j] + ';';
+                        }
+                        css += '"';
+                    }
+                    $parent.html("<div class='BlockContent'" + css + ">" + message + "</div>");
+                    $parent.children().click(function(e) {
+                        $(this).addClass('closed');
+                        $(this).fadeOut('medium');
+                    });
+                });
+            }
+        }, delay);
+        delay += step;
     }
 }
 
@@ -66,8 +115,8 @@ function addStyles() {
     var styleEl = document.createElement('style');
     document.head.appendChild(styleEl);
     var styleSheet = styleEl.sheet;
-    styleSheet.insertRule(".BlockContent {text-align: center;font-size: 1.5em;background-color: rgba(146, 32, 32, 0.46);color: black;padding: 5px 0px;margin-bottom: 15px;border: 1px solid red;cursor: pointer; transition: 0.3s linear all;}", styleSheet.cssRules.length);
-    styleSheet.insertRule(".BlockContent:hover:not(.closed) {background-color: rgba(146, 32, 32, 0.76);}", styleSheet.cssRules.length);
+    styleSheet.insertRule(".BlockContent {text-align: center;font-size: 1.5em;background-color: rgb(146, 32, 32); opacity: 0.46; color: black;padding: 5px 0px;margin-bottom: 15px;border: 1px solid red;cursor: pointer; transition: 0.3s linear all;}", styleSheet.cssRules.length);
+    styleSheet.insertRule(".BlockContent:hover:not(.closed) { opacity: 0.76;}", styleSheet.cssRules.length);
     styleSheet.insertRule(".BlockContent.closed {cursor: default;}", styleSheet.cssRules.length);
 }
 
