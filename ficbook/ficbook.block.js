e520a70177434b39f1b64d10b9f0a50fb1d6fdee
ShadowOfKing <ShadowKing65@mail.ru>
2018-02-21 14:07:21 +0500

    Update ficbook.block.js

--- ficbook/ficbook.block.js
+++ ficbook/ficbook.block.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ficbook.block
 // @namespace    https://github.com/ShadowOfKing/JSScripts/
-// @version      1.2
+// @version      1.3
 // @description  Скрывает на сайте элементы, которые чем-то не угодили. Например фанфики, со слэшем, фэмслэшем, с нелюбимыми жанрами... Или рекламу.
 // @author       Wilat Collany
 // @match        https://ficbook.net/*
@@ -57,7 +57,8 @@ var blocks = {
         "css": {
             "background-color": "#4d2917",
             "color": "#f6ecda",
-        }
+        },
+        "saveContent": true
     },
     "genres": {
         "constraints":
@@ -72,7 +73,8 @@ var blocks = {
         "css": {
             "background-color": "#2c1a14",
             "color": "#dd3131",
-        }
+        },
+        "saveContent": true
     },
 };
 
@@ -82,12 +84,16 @@ function blockElement(el) {
     var delay = el.delay == null ? 1 : el.delay;
     var count = el.repeats == null ? 1 : el.repeats;
     var step = el.step == null ? delay : el.step;
+    var saveContent = el.saveContent;
     for (var iter = 0; iter < count; iter++) {
         setTimeout(function() {
             for (var i = 0; i < el.constraints.length; i++) {
                 var con = el.constraints[i];
                 var $selector = $(con.selector);
                 $selector.each(function() {
+                    if ($(this).parents('.blockedContent').length > 0) {
+                        return;
+                    }
                     var $parent = $(this).parents(con.parent).first();
                     $parent.wrap("<div></div>");
                     $parent = $parent.parent();
@@ -99,10 +105,26 @@ function blockElement(el) {
                         }
                         css += '"';
                     }
-                    $parent.html("<div class='BlockContent'" + css + ">" + message + "</div>");
-                    $parent.children().click(function(e) {
+                    if (saveContent) {
+                        $parent.html("<div style='display: none;' class='blockedContent'>" + $parent.html() + "</div>");
+                        $parent.append('<div class="openBlockedContent">Показать скрытый контент</div>');
+                        $parent.css('position', 'relative');
+                    } else {
+                        $parent.html("<span></span>");
+                    }
+                    $parent.children().last().before("<div class='BlockContent'" + css + ">" + message + "</div>");
+                    $parent.children('.BlockContent').click(function(e) {
                         $(this).addClass('closed');
-                        $(this).fadeOut('medium');
+                        $(this).parent().slideUp(500);
+                    });
+                    $parent.children('.openBlockedContent').click(function(e) {
+                        $(this).hide();
+                        var $el = $(this).parent().slideUp(500);
+                        setTimeout(function() {
+                            $el.children('.BlockContent').hide();
+                            $el.children('.blockedContent').show();
+                            $el.slideDown(500);
+                        }, 500);
                     });
                 });
             }
@@ -118,6 +140,8 @@ function addStyles() {
     styleSheet.insertRule(".BlockContent {text-align: center;font-size: 1.5em;background-color: rgb(146, 32, 32); opacity: 0.46; color: black;padding: 5px 0px;margin-bottom: 15px;border: 1px solid red;cursor: pointer; transition: 0.3s linear all;}", styleSheet.cssRules.length);
     styleSheet.insertRule(".BlockContent:hover:not(.closed) { opacity: 0.76;}", styleSheet.cssRules.length);
     styleSheet.insertRule(".BlockContent.closed {cursor: default;}", styleSheet.cssRules.length);
+    styleSheet.insertRule(".openBlockedContent {transition: 0.5s ease-in-out all; position: absolute;color: #fafafa;right: 5px;bottom: 5px;background-color: #121517;padding: 5px;border-radius: 15px;border: 1px solid red; opacity: 0.7; cursor: pointer;}", styleSheet.cssRules.length);
+    styleSheet.insertRule(".openBlockedContent:hover {opacity: 1.7; background-color: #fafafa; color: #121517;}", styleSheet.cssRules.length);
 }
 
 (function() {
