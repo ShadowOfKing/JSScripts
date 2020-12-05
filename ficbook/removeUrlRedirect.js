1f06c7ee123312628a1e53f3479c99ea34955f30
ShadowOfKing <ShadowKing65@mail.ru>
2020-12-06 02:10:21 +0500

    Create removeUrlRedirect.js

---
+++ ficbook/removeUrlRedirect.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name         fanfiction.addon
+// @namespace    https://github.com/ShadowOfKing/JSScripts/
+// @version      1.0.0
+// @description  Удаляет переадресацию на страницу "Вы уверены, что хотите перейти по ссылки?"
+// @author       Wilat Collany
+// @include      https://ficbook.net/*
+// @include      https://www.ficbook.net/*
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/ShadowOfKing/JSScripts/master/ficbook/removeUrlRedirect.js
+// @downloadURL  https://raw.githubusercontent.com/ShadowOfKing/JSScripts/master/ficbook/removeUrlRedirect.js
+// ==/UserScript==
+
+// Устанавливаем интервал запуска алгоритма поиска и замены ссылок - нужно для работы с подружающимися комментариями
+const linksCheckInterval = 10;
+
+const siteUrl = 'https://ficbook.net'
+const relativePageUrl = '/away?'
+const fullPageUrl = `${siteUrl}${relativePageUrl}`
+const replacedLinkClassName = 'WilatCssSriptsFicbookRemoveUrlRedirect'
+
+(() => {
+  'use strict'
+  setInterval(() => 
+    Array
+      .from(document.getElementsByTagName('a'))
+      .filter(a => {
+        const href = a.href || ''
+        return !a.classList.contains(replacedLinkClassName) 
+          && (href.startsWith(fullPageUrl) || href.startsWith(relativePageUrl))
+          && (a.text || '').trim().startsWith('http')
+      }).forEach(a => {
+        a.href = a.text.trim()
+        a.classList.add(replacedLinkClassName)
+      }), linksCheckInterval);
+})()
